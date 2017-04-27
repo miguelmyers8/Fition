60cac427726dad24eedfb2185c0be134edca4162
miguel myers <myers.m007@gmail.com>
2017-04-27 19:20:38 -0400

    google

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,15 +25,15 @@ end
     @post = Post.find(params[:id])
     x = @post.image.url
 
-  #private_key =  Google::Auth::ServiceAccountCredentials.make_creds(
-      #scope: 'https://www.googleapis.com/auth/cloud-platform',
-      #json_key_io: StringIO.new(ENV['VISION_KEYFILE_JSON'])
-    #)
+  private_key =  Google::Auth::ServiceAccountCredentials.make_creds(
+      scope: 'https://www.googleapis.com/auth/cloud-platform',
+      json_key_io: StringIO.new(ENV['VISION_KEYFILE_JSON'])
+    )
 
 
     vision = Google::Cloud::Vision.new(
             project: "redoproject-163021",
-            keyfile: "config/redoproject-e87605fb29d9.json",
+            keyfile: private_key,
             )
              @image = vision.image(x)
              @labels = @image.labels
